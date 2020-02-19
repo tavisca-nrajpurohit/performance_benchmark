d248d0981ea04eea43e671007be44c1f4f83c450
skapoor <skapoor@affinionds.com>
2020-02-19 10:57:21 +0530

    script template changes

--- AngularSampleApp/scripts/automate_performance.ts
+++ AngularSampleApp/scripts/automate_performance.ts
@@ -5,7 +5,7 @@ var fs= require('fs');
 function automateTest() {
     //console.log("checkkk", compo);
     for(var comp in compo) {
-        let data = fs.readFileSync('dist/test_markup.html');
+        let data = fs.readFileSync('scripts/test_markup.html');
         let file_content = data.toString();
         file_content = file_content.replace("[Placeholder for markup]", compo[comp]);
         fs.writeFile('dist/audit-'+comp+'.html', file_content, (err) => { 
